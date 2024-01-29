43bcaa5c1d3d36ec5fb69f2c836133fef4a9f229
Daniele Castrovinci <daniele.castrovinci@lutech.it>
2024-01-29 17:56:20 +0100

    vd

---
+++ src/views/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+     ADMIN LOGIN
+    </v-responsive>
+  </v-container>
+</template>
+<script>
+export default {};
+</script>
+<style>
+ 
+</style>
